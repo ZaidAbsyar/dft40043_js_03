0b469e64b142ee6314bf49f40208c12854155798
ZaidAbsyar <zaidabsyar43@gmail.com>
2021-11-19 17:02:51 +0800

    darab

---
+++ script.js
@@ -0,0 +1,12 @@
+function darab() {
+    var number;
+    var result = "<table>";
+
+    number = Number(document.getElementById("number").value);
+
+    for (var i = 1; i <= 11; i++) {
+        result = result + "<tr><td>" + number + "</td><td>X</td><td>" + i + "</td><td>=</td><td>" + number * i + "</td></tr>";
+    }
+    result += "</table>";
+    document.getElementById("result").innerHTML = result;
+}
